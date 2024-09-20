a05a88d75d9d3bab788216775e8e94304f331c3e
seasalt <654765554@qq.com>
2024-09-20 15:10:05 +0800

    test

--- shell.sh
+++ shell.sh
@@ -2,5 +2,5 @@ echo hello world
 
 current_path=$(pwd)  
 echo "当前路径是：$current_path"
-
-ruff check model
+ls /home/runner/work/test/test/.github/workflows/actions/install_dependencies
+# ruff check model
